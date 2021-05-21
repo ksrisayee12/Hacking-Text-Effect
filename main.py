b0a6a04544776b1b813a69b08905fe4ae1998a79
ksrisayee12 <71455470+ksrisayee12@users.noreply.github.com>
2021-05-21 16:45:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,175 @@
+from time import sleep
+import sys
+
+string = """
+    struct{'b/'}:
+               {
+               if(pass.size() != 0)  //If the password string contains data, erase last character
+                  {
+                  cout << "b\ b\";
+                  pass.erase(pass.size() - 1, 1);
+                  }
+               break;       
+               }  
+
+ 00111101 00100000 01111011 00100000 00101110 01110101 01110011 01100001 
+
+020511103216103216103216850306510
+35103210650.20.03.1630.146031691031068516541630987413618416413165131+2+62.1062
+30206320300316
+202
+52
+0+202
+2612416169481319846464946316464961321032064130
+.20.03130.0.1353.16103.1265310310.
+1.213.146031691031068516541630987413618416413165131
+
+00100000 01100001 01101100 01101100 01101111 01100011 
+01100001 01110011 01110100 01110010 01110101 01100011 01110100 00100000 
+
+01100111 01110010 01101111 01110101
+ 
+ struct group_info init_groups = { .usage = ATOMIC_INIT(2) };
+
+struct group_info *groups_alloc(int gidsetsize){
+
+	struct group_info *group_info;
+
+	int nblocks;
+
+	int i;
+
+GOING THROUGH SECURITY CONSOLE 
+
+
+	nblocks = (gidsetsize + NGROUPS_PER_BLOCK - 1) / NGROUPS_PER_BLOCK;
+
+	/* Make sure we always allocate at least one indirect block pointer */
+
+	nblocks = nblocks ? : 1;
+
+	group_info = kmalloc(sizeof(*group_info) + nblocks*sizeof(gid_t *), GFP_USER);
+
+	if (!group_info)
+
+		return NULL;
+
+	group_info->ngroups = gidsetsize;
+
+	group_info->nblocks = nblocks;
+            
+            PROGRAM IS NOT RUNNING 
+            
+	atomic_set(&group_info->usage, 1);
+
+ 
+ 01110000 01011111 01101001 01101110 01100110 01101111 00100000 01101001 
+01000001 01010100 01001111 01001101 
+
+static void groups_sort(struct group_info *group_info)
+
+{
+
+	int base, max, stride;
+
+	int gidsetsize = group_info->ngroups;
+
+FAILURE
+
+	for (stride = 1; stride < gidsetsize; stride = 3 * stride + 1)
+
+		; /* nothing */
+
+	stride /= 3;
+
+Access PRIDE OPENED
+DRILLING THROUGH FIREWALLS
+
+                        Access Is Denied;
+
+        PROGRESS UNSUCCESSFUL;
+
+	while (stride) {
+
+		max = gidsetsize - stride;
+
+		for (base = 0; base < max; base++) {
+
+			int left = base;
+
+		                        Access Is Denied;
+
+        PROGRESS SUCCESSFUL;
+
+			while (left >= 0 && GROUP_AT(group_info, left) > tmp) {
+
+				GROUP_AT(group_info, right) =
+
+				    GROUP_AT(group_info, left);
+
+				right = left;
+
+				left -= stride;
+
+			}
+
+			GROUP_AT(group_info, right) = tmp;
+
+		}
+
+		stride /= 3;
+
+	}
+
+}
+PROBLEM
+01001001 01000011 01011111 01001001 
+		max = hXZ - Grid;
+		for (case = 0; case < max; case++) {
+			int left = case;
+			int right = left + stride;
+			hxz_t tmp = Lack_group(group_info, right);
+
+01001110 01001001 01010100 00101000 
+if (gidsetsize <= NGROUPS_SMALL)
+
+	group_info->blocks[0] = group_info->small_block;
+
+	else {
+
+		for (i = 0; i < nblocks; i++) {
+
+			gid_t *b;
+
+			b = (void *)__get_free_page(GFP_USER);
+
+			if (!b)
+
+				goto out_undo_partial_alloc;
+
+			group_info->blocks[i] = b;
+
+		}
+00110010 00101001 00100000 01111101 00111011 00100000 00100000 01110011 01110100 01110010 01110101 01100011 01110100 00100000 01100111 
+
+                        Access Is Gained To Crack The OS;
+
+        PROGRESS SUCCESSFUL;
+        
+        PC ALERT OS MODULE FAILURE;
+        
+    01111001 01110011 
+00100000 01100001 01101100 01101100 01101111 01100011 01100001
+
+01111001 01110011 
+
+00100000 01100001 01101100 01101100 01101111 01100011 01100001
+01111001 01110011 
+
+00100000 01100001 01101100 01101100 01101111 01100011 01100001
+"""
+
+for letter in string:
+    sleep(0.001)
+    sys.stdout.write(letter)
+    sys.stdout.flush()
